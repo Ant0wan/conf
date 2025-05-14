5bf65591f2bd1d329b86193681b6450f546ba608
Antoine Barthelemy <antoine@linux.com>
2025-05-14 11:34:55 +0200

    no url init.sh

--- scripts/ohmyposh.sh
+++ scripts/ohmyposh.sh
@@ -3,9 +3,15 @@ set -o errexit
 set -o nounset
 OS="$(uname -s | awk '{print tolower($0)}')"
 ARCH="$(uname -m | sed 's/x86_64/amd64/')"
-githubsource="https://raw.githubusercontent.com/Ant0wan/conf/main/"
-sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-${OS}-${ARCH} -O /usr/local/bin/oh-my-posh
+sudo wget "https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-${OS}-${ARCH}" -O /usr/local/bin/oh-my-posh
 sudo chmod +x /usr/local/bin/oh-my-posh
 oh-my-posh font install Meslo
 oh-my-posh get shell
-wget "${githubsource}theme.omp.json" -O "$HOME"/.theme.omp.json
+if [ "$(basename $(pwd))" = "scripts" ]; then
+	cp "$(dirname $(pwd))"/theme.omp.json "$HOME"/.theme.omp.json
+elif test -e "$0"; then
+	cp theme.omp.json "$HOME"/.theme.omp.json
+else
+	githubsource="https://raw.githubusercontent.com/Ant0wan/conf/main/"
+	wget "${githubsource}theme.omp.json" -O "$HOME"/.theme.omp.json
+fi
