d00f3c67528be7b9c8097f3157f074118c11e9cc
Antoine Barthelemy <antoine@linux.com>
2025-06-14 15:25:34 +0200

    fix

--- scripts/ohmyposh.sh
+++ scripts/ohmyposh.sh
@@ -5,7 +5,6 @@ OS="$(uname -s | awk '{print tolower($0)}')"
 ARCH="$(uname -m | sed 's/x86_64/amd64/')"
 sudo wget "https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-${OS}-${ARCH}" -O /usr/local/bin/oh-my-posh
 sudo chmod +x /usr/local/bin/oh-my-posh
-#oh-my-posh font install Meslo
 oh-my-posh get shell
 if [ "$(basename $(pwd))" = "scripts" ]; then
 	cp "$(dirname $(pwd))"/theme.omp.json "$HOME"/.theme.omp.json
