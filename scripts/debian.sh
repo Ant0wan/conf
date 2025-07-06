f090a5ea719363cb47280b5200d0ebde49ba200c
Antoine Barthelemy <antoine@linux.com>
2025-07-06 12:59:50 -0400

    Create debian.sh

---
+++ scripts/debian.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+dconf write /org/gnome/desktop/sound/event-sounds "false" 
+su -c - root 'echo "antoine ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/antoine'
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+gsettings set org.gnome.Terminal.Legacy.Settings theme-variant 'dark'
+su -c - root '
+echo "deb https://deb.debian.org/debian/ bookworm non-free contrib main non-free-firmware
+deb-src https://deb.debian.org/debian/ bookworm non-free contrib main non-free-firmware #Added by software-properties
+deb https://deb.debian.org/debian/ bookworm-proposed-updates main non-free-firmware non-free contrib
+" > /etc/apt/sources.list && apt update'
