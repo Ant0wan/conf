b079f0f772a590d7f403c982cfae1de5df394f4e
Antoine Barthelemy <antoine@linux.com>
2025-07-06 13:04:48 -0400

    Update debian.sh

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -8,3 +8,4 @@ echo "deb https://deb.debian.org/debian/ bookworm non-free contrib main non-free
 deb-src https://deb.debian.org/debian/ bookworm non-free contrib main non-free-firmware #Added by software-properties
 deb https://deb.debian.org/debian/ bookworm-proposed-updates main non-free-firmware non-free contrib
 " > /etc/apt/sources.list && apt update'
+su - -c root 'timedatectl set-timezone Europe/Paris'
