4c65b3d3c651f0c7c77206cbe0160483462873cb
Antoine Barthelemy <antoine@linux.com>
2026-03-13 18:01:24 +0100

    feat(trivy): dnf and apt support

--- scripts/trivy.sh
+++ scripts/trivy.sh
@@ -5,7 +5,18 @@ SCRIPTNAME="$(basename "$0")"
 BIN="${SCRIPTNAME%.sh}"
 TMP="/tmp/${BIN}_${VERSION}"
 OS="$(uname -s)"
-TARGET="trivy_${VERSION#?}_${OS}-64bit.deb"
 mkdir -p "$TMP"
+if [ -f /etc/fedora-release ]; then
+	TARGET="trivy_${VERSION#?}_${OS}-64bit.rpm"
+elif [ -f /etc/lsb-release ] || [ -f /etc/os-release ] ; then
+	TARGET="trivy_${VERSION#?}_${OS}-64bit.deb"
+else
+	echo "Unsupported distribution. This script only supports Fedora and Ubuntu."
+	exit 1
+fi
 wget "https://github.com/aquasecurity/trivy/releases/download/${VERSION}/${TARGET}" -O "${TMP}/${TARGET}"
-sudo dpkg -i "${TMP}/${TARGET}"
+if [ -f /etc/fedora-release ]; then
+	sudo dnf --assumeyes --quiet install "${TMP}/${TARGET}"
+else [ -f /etc/lsb-release ] || [ -f /etc/os-release ]
+	sudo dpkg -i "${TMP}/${TARGET}"
+fi
