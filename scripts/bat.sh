bff8f381a522114566ed43768a42222a28b156c3
Antoine Barthelemy <antoine@linux.com>
2026-03-29 11:43:10 +0200

    feat(scripts): no package manager needed

--- scripts/bat.sh
+++ scripts/bat.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
-if [ -f /etc/fedora-release ]; then
-  sudo dnf install -y bat
-elif [ -f /etc/lsb-release ] || [ -f /etc/os-release ] ; then
-  sudo apt-get update
-  sudo apt-get install -y bat
-else
-  echo "Unsupported distribution. This script only supports Fedora and Ubuntu."
-  exit 1
-fi
-echo "Bat has been successfully installed."
+set -o errexit
+REPO="sharkdp/bat"
+VERSION=$(curl -s "https://api.github.com/repos/${REPO}/releases/latest" | jq -r '.tag_name')
+ARCH="$(uname -m)"
+OS="$(uname -s | tr '[:upper:]' '[:lower:]')"
+case "$OS" in
+  linux)
+    OS="unknown-linux-musl"
+    ;;
+  darwin)
+    OS="apple-darwin"
+    ;;
+  *)
+    echo "Unsupported OS: $OS"
+    exit 1
+    ;;
+esac
+TARGET="bat-${VERSION}-${ARCH}-${OS}.tar.gz"
+TMP="/tmp/bat"
+mkdir -p "$TMP"
+curl -sL "https://github.com/${REPO}/releases/download/${VERSION}/${TARGET}" -o "${TMP}/${TARGET}"
+sudo tar -xzf "${TMP}/${TARGET}" -C "$TMP" --strip-components=1 "bat-${VERSION}-${ARCH}-${OS}/bat"
+sudo install "$TMP/bat" /usr/local/bin/
