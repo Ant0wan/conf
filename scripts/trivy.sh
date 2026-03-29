4e3d6bb1faf5131ed768e5cd6b71c3256a283803
Antoine Barthelemy <antoine@linux.com>
2026-03-29 11:56:41 +0200

    feat(scripts): remove chrome and no distro trivy

--- scripts/trivy.sh
+++ scripts/trivy.sh
@@ -1,22 +1,12 @@
 #!/bin/sh
 set -o errexit
-VERSION=$(curl -s "https://api.github.com/repos/aquasecurity/trivy/releases/latest" | jq -r '.tag_name')
-SCRIPTNAME="$(basename "$0")"
-BIN="${SCRIPTNAME%.sh}"
-TMP="/tmp/${BIN}_${VERSION}"
-OS="$(uname -s)"
+REPO="aquasecurity/trivy"
+VERSION=$(curl -s "https://api.github.com/repos/${REPO}/releases/latest" | jq -r '.tag_name' | tr -d 'v')
+ARCH="$(uname -m | sed 's/x86_64/64bit/; s/aarch64/ARM64/')"
+OS="$(uname -s | tr '[:upper:]' '[:lower:]' | sed 's/linux/Linux/; s/darwin/macOS/')"
+TARGET="trivy_${VERSION}_${OS}-${ARCH}.tar.gz"
+TMP="/tmp/trivy"
 mkdir -p "$TMP"
-if [ -f /etc/fedora-release ]; then
-	TARGET="trivy_${VERSION#?}_${OS}-64bit.rpm"
-elif [ -f /etc/lsb-release ] || [ -f /etc/os-release ] ; then
-	TARGET="trivy_${VERSION#?}_${OS}-64bit.deb"
-else
-	echo "Unsupported distribution. This script only supports Fedora and Ubuntu."
-	exit 1
-fi
-wget "https://github.com/aquasecurity/trivy/releases/download/${VERSION}/${TARGET}" -O "${TMP}/${TARGET}"
-if [ -f /etc/fedora-release ]; then
-	sudo dnf --assumeyes --quiet install "${TMP}/${TARGET}"
-else [ -f /etc/lsb-release ] || [ -f /etc/os-release ]
-	sudo dpkg -i "${TMP}/${TARGET}"
-fi
+curl -sL "https://github.com/${REPO}/releases/download/v${VERSION}/${TARGET}" -o "${TMP}/${TARGET}"
+sudo tar -xzf "${TMP}/${TARGET}" -C "$TMP" trivy
+sudo install "$TMP/trivy" /usr/local/bin/
